09be0fb805b6e458f08d80c6f7a5589f3e7c7028
swati1751 <92619834+swati1751@users.noreply.github.com>
2021-10-24 15:28:27 +0100

    leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,49 @@
+package calculator;
+
+import java.io.*;
+
+public class leapyear {
+
+    public static void isLeapYear(int year) {
+
+            boolean is_leap_year = false;
+
+            if (year % 4 == 0) {
+
+                if (year % 100 == 0) {
+
+
+                    if (year % 400 == 0) {
+                        is_leap_year = true;
+                    }
+
+                    else {
+                        is_leap_year = false;
+                    }
+                }
+                is_leap_year = true;
+            }
+
+            // We land here when corresponding if fails
+            // If year is not divisible by 4
+            else
+
+                is_leap_year = false;
+
+            if (!is_leap_year) {
+                System.out.println(year + " : Non Leap-year");
+            }
+
+            else {
+                System.out.println(year + " : Leap-year");
+            }
+        }
+
+        public static void main(String[] args)
+        {
+            isLeapYear(2000);
+
+            isLeapYear(2002);
+        }
+    }
+
