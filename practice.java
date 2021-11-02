1eeb813e43381b8c32d46b1090c9267da20355cc
swati1751 <92619834+swati1751@users.noreply.github.com>
2021-11-02 15:46:48 +0000

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,47 @@
+package practiceassignment3;
+
+abstract class Parent {
+
+        void message() { }
+
+    }
+
+    class Subclass1 extends Parent{
+
+        void message() {
+
+            System.out.println("This is First subclass.");
+
+        }
+
+    }
+
+    class Subclass2 extends Parent{
+
+        void message() {
+
+            System.out.println("This is Second subclass.");
+
+        }
+
+    }
+
+    public class practice{
+
+        public static void main(String args[]) {
+
+            Subclass1 sb1 = new Subclass1();
+
+            Subclass2 sb2 = new Subclass2();
+
+            sb1.message();
+
+            sb2.message();
+
+        }
+
+    }
+
+
+
+
