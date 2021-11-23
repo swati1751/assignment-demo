d819398338218ca913f5d7017c29705f3ac73e38
swati1751 <92619834+swati1751@users.noreply.github.com>
2021-11-23 23:44:27 +0530

    NumberStartingWith1UsingStream.java

---
+++ NumberStartingWith1UsingStream.java
@@ -0,0 +1,16 @@
+package assignment7;
+
+    import java.util.*;
+import java.util.stream.*;
+
+public class NumberStartingWith1UsingStream {
+
+        public static void main(String args[]) {
+            List<Integer> myList = Arrays.asList(10,15,8,49,25,98,32);
+            myList.stream()
+                    .map(s -> s + "") // Convert integer to String
+                    .filter(s -> s.startsWith("1"))
+                    .forEach(System.out::println);
+        }
+    }
+
