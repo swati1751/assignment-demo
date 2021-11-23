ae910c5a4064a75764240cb726280acdc30e474a
swati1751 <92619834+swati1751@users.noreply.github.com>
2021-11-23 23:41:37 +0530

    EvenNumberUsingStream.java

---
+++ EvenNumberUsingStream.java
@@ -0,0 +1,15 @@
+package assignment7;
+
+    import java.util.*;
+    import java.util.stream.*;
+
+public class EvenNumberUsingStream {
+
+        public static void main(String args[]) {
+            List<Integer> myList = Arrays.asList(10,15,8,49,25,98,32);
+            myList.stream()
+                    .filter(n -> n%2 == 0)
+                    .forEach(System.out::println);
+        }
+    }
+
