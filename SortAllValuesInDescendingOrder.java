7235f3beaba232bc94dee4852a741123be0131bd
swati1751 <92619834+swati1751@users.noreply.github.com>
2021-11-23 23:55:14 +0530

    SortAllValuesInDescendingOrder.java

---
+++ SortAllValuesInDescendingOrder.java
@@ -0,0 +1,17 @@
+package assignment7;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.Function;
+
+public class SortAllValuesInDescendingOrder {
+
+        public static void main(String args[]) {
+            List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
+
+            myList.stream()
+                    .sorted(Collections.reverseOrder())
+                    .forEach(System.out::println);
+        }
+    }
+
