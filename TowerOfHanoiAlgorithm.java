82b8d82438b9bb66985f3fc0e12812b7115ce375
swati1751 <92619834+swati1751@users.noreply.github.com>
2021-12-10 10:26:49 +0530

    TowerOfHanoiAlgorithm.java

---
+++ TowerOfHanoiAlgorithm.java
@@ -0,0 +1,26 @@
+package DsaAssignment;
+
+public class TowerOfHanoiAlgorithm{
+
+
+        static void TowerOfHanoiAlgorithm(int n, char from_rod, char to_rod, char aux_rod)
+        {
+            if (n == 1)
+            {
+                System.out.println("Move disk 1 from rod "+
+                        from_rod+" to rod "+to_rod);
+                return;
+            }
+            TowerOfHanoiAlgorithm(n - 1, from_rod, aux_rod, to_rod);
+            System.out.println("Move disk "+ n + " from rod " +
+                    from_rod +" to rod " + to_rod );
+            TowerOfHanoiAlgorithm(n - 1, aux_rod, to_rod, from_rod);
+        }
+
+        public static void main(String args[])
+        {
+            int n = 4;
+            TowerOfHanoiAlgorithm(n, 'A', 'C', 'B'); // A, B and C are names of rods
+        }
+    }
+
