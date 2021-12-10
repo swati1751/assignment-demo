49dded22b0870fa12acb1c0486ea5cf5f88ebcb8
swati1751 <92619834+swati1751@users.noreply.github.com>
2021-12-10 10:25:41 +0530

    FirstNonRepeatedCharacter.java

---
+++ FirstNonRepeatedCharacter.java
@@ -0,0 +1,42 @@
+package DsaAssignment;
+
+public class FirstNonRepeatedCharacter {
+
+        static final int NO_OF_CHARS = 256;
+        static char count[] = new char[NO_OF_CHARS];
+
+
+        static void getCharCountArray(String str)
+        {
+            for (int i = 0; i < str.length(); i++)
+                count[str.charAt(i)]++;
+        }
+
+        static int firstNonRepeatingCharacter(String str)
+        {
+            getCharCountArray(str);
+            int index = -1, i;
+
+            for (i = 0; i < str.length(); i++) {
+                if (count[str.charAt(i)] == 1) {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+        public static void main(String[] args)
+        {
+            String str = "geeksforgeeks";
+            int index = firstNonRepeatingCharacter(str);
+
+            System.out.println(
+                    index == -1
+                            ? "Either all characters are repeating or string "
+                            + "is empty"
+                            : "First non-repeating character is "
+                            + str.charAt(index));
+        }
+    }
+
